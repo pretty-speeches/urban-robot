146803eccb603a4bad2720e8b05e07bee3f18e2e
nathalie <nathalie.hauser@bluevine.com>
2017-10-12 14:30:56 +0300

    wip

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='urbanrobot',
+      version='0.1',
+      description='print something',
+      url='https://github.com/pretty-speeches/urban-robot',
+      author='Pretty Speeches',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['urban-robot'],
+      zip_safe=False)
